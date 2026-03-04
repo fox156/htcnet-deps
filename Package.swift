4bb2f89be7855c86c8d858fb1ae99e930bcf76e3
Eternity <eternity@htc.local>
2026-03-04 10:13:23 +0000

    Promise types inside include guard

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 
 let githubBaseURL = "https://github.com/fox156/htcnet-deps/releases/download"
 
-let libsignalVersion = "libsignal-v0.86.16c"
-let signalFfiChecksum = "581bd5838c155e2a4f3296a29aebf29e8305dcd16f1006ed49e2255e5e159698"
+let libsignalVersion = "libsignal-v0.86.16d"
+let signalFfiChecksum = "b548d5b1144ec85d9fdc8ef02c461602df3ce4e7b36f022f5409e694f69982d1"
 
 let ringrtcVersion = "ringrtc-v2.64.1"
 let ringRTCChecksum = "775cf83d086aa15baaeb912d3b62483491036f6a695ca8dc8057606e8a2732c8"
