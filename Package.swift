a44e68248f4baf31450a26fcd3f8392f6b32ab5d
Eternity <eternity@htc.local>
2026-03-06 13:08:43 +0000

    Replace SignalRingRTC with stasel/WebRTC M141

--- Package.swift
+++ Package.swift
@@ -6,10 +6,6 @@ let githubBaseURL = "https://github.com/fox156/htcnet-deps/releases/download"
 let libsignalVersion = "libsignal-v0.86.16i"
 let signalFfiChecksum = "2f6ec0d95c7e78e29ca531086436c7d80629272329eb97e0505c8c48f8cb9cdb"
 
-let ringrtcVersion = "ringrtc-v2.64.1"
-let ringRTCChecksum = "775cf83d086aa15baaeb912d3b62483491036f6a695ca8dc8057606e8a2732c8"
-let webRTCChecksum = "d6fcb8aec002f769b2987c0ac372a04d9a553b08f3fc61020dedf3acc68ffd4a"
-
 let sqlcipherVersion = "sqlcipher-v4.6.1-fts5b"
 let sqlcipherChecksum = "b9463fa3ba3a680b8b29dbff6ad870ca003520f1160e8759c29df60a2218fc46"
 
@@ -18,24 +14,16 @@ let package = Package(
     platforms: [.iOS(.v16)],
     products: [
         .library(name: "LibSignalClient", targets: ["LibSignalClient"]),
-        .library(name: "SignalRingRTC", targets: ["SignalRingRTC"]),
         .library(name: "GRDB", targets: ["GRDB"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/stasel/WebRTC.git", exact: "141.0.0"),
     ],
     targets: [
         .binaryTarget(
             name: "SignalFfi",
             url: "\(githubBaseURL)/\(libsignalVersion)/LibSignalFFI.xcframework.zip",
             checksum: signalFfiChecksum
-        ),
-        .binaryTarget(
-            name: "RingRTC",
-            url: "\(githubBaseURL)/\(ringrtcVersion)/LibRingRTC.xcframework.zip",
-            checksum: ringRTCChecksum
-        ),
-        .binaryTarget(
-            name: "WebRTC",
-            url: "\(githubBaseURL)/\(ringrtcVersion)/WebRTC.xcframework.zip",
-            checksum: webRTCChecksum
         ),
         .binaryTarget(
             name: "GRDBSQLCipher",
@@ -49,11 +37,6 @@ let package = Package(
             swiftSettings: [
                 .define("SIGNAL_MEDIA_SUPPORTED", .when(platforms: [.iOS])),
             ]
-        ),
-        .target(
-            name: "SignalRingRTC",
-            dependencies: ["RingRTC", "WebRTC"],
-            path: "Sources/SignalRingRTC"
         ),
         .target(
             name: "GRDB",
